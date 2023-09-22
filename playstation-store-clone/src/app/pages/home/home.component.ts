6042505e09d9774a5e773ec80fb0da9c88749f05
Thiago Gonçalves <mg.az.ul@hotmail.com>
2023-09-21 22:01:35 -0300

    Home component grid layout

--- playstation-store-clone/src/app/pages/home/home.component.ts
+++ playstation-store-clone/src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { game } from '../../types/game';
+import { GameService } from './../../services/game.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  games: game[] = [];
+  constructor(private gameService: GameService) {}
 
+  ngOnInit(): void {
+    this.games = this.gameService.getGames();
+    console.log(this.games);
+  }
 }
--- playstation-store-clone/src/app/services/game.service.ts
+++ playstation-store-clone/src/app/services/game.service.ts
@@ -14,7 +14,7 @@ export class GameService {
       label: 'Exclusive',
       title: 'Play it Now!',
       type: 'DIGITAL PS5',
-      price: 'U$ 199.29',
+      price: 'US$ 199.29',
     };
 
     const obj2: game = {
@@ -23,7 +23,7 @@ export class GameService {
       label: 'Exclusive',
       title: 'Play it Now!',
       type: 'DIGITAL PS5',
-      price: 'U$ 199.29',
+      price: 'US$ 199.29',
     };
 
     const obj3: game = {
@@ -32,7 +32,7 @@ export class GameService {
       label: 'Exclusive',
       title: 'Play it Now!',
       type: 'DIGITAL PS5',
-      price: 'U$ 199.29',
+      price: 'US$ 199.29',
     };
 
     const obj4: game = {
@@ -41,10 +41,95 @@ export class GameService {
       label: 'Exclusive',
       title: 'Play it Now!',
       type: 'DIGITAL PS5',
-      price: 'U$ 199.29',
+      price: 'US$ 199.29',
     };
 
-    const games: game[] = [obj1, obj2, obj3, obj4];
+    const obj5: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj6: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj7: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj8: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj9: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj10: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj11: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const obj12: game = {
+      imgUrl:
+        'https://cdn.awsli.com.br/300x300/2154/2154653/produto/131161269/56048b0da8.jpg',
+      label: 'Exclusive',
+      title: 'Play it Now!',
+      type: 'DIGITAL PS5',
+      price: 'US$ 199.29',
+    };
+
+    const games: game[] = [
+      obj1,
+      obj2,
+      obj3,
+      obj4,
+      obj5,
+      obj6,
+      obj7,
+      obj8,
+      obj9,
+      obj10,
+      obj11,
+      obj12,
+    ];
     return games;
   }
 }
